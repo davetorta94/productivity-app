8b21c958be7b3d1eb3144ccb5b2e203b58ce8277
David <david.torta.olah@gmail.com>
2023-01-23 19:32:43 +0100

    error corregido TodoCategories

--- src/TodoCategories.jsx
+++ src/TodoCategories.jsx
@@ -1,32 +1,32 @@
-//import { Link } from "react-router-dom"
-//
-//export const TodoCategories = () => {
-//
-//  const marcarCategoria = () => {
-//    console.log('categoria marcada')
-//  }
-//  return (
-//    <>
-//    
-//    <div className="dropdown">
-//  <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-//    Categorias
-//  </button>
-//  <ul className="dropdown-menu dropdown-menu-dark">
-//    <li><Link className="dropdown-item" onClick={marcarCategoria}>Trabajo</Link></li>
-//    <li><Link className="dropdown-item" onClick={marcarCategoria}>Hogar</Link></li>
-//    <li><Link className="dropdown-item" onClick={marcarCategoria}>Ocio</Link></li>
-//  </ul>
-//</div>
-//    </>
-//   
-//      
-//  )
-//}
-//
-//
-//
-//
+import { Link } from "react-router-dom"
+
+export const TodoCategories = () => {
+
+  const marcarCategoria = () => {
+    console.log('categoria marcada')
+  }
+  return (
+    <>
+    
+    <div className="dropdown">
+  <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Categorias
+  </button>
+  <ul className="dropdown-menu dropdown-menu-dark">
+    <li><Link className="dropdown-item" onClick={marcarCategoria}>Trabajo</Link></li>
+    <li><Link className="dropdown-item" onClick={marcarCategoria}>Hogar</Link></li>
+    <li><Link className="dropdown-item" onClick={marcarCategoria}>Ocio</Link></li>
+  </ul>
+</div>
+    </>
+   
+      
+  )
+}
+
+
+
+
 
 
 
